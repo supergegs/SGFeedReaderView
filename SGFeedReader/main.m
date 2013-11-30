30fe27445473dc0cb6779430ef416d8f9ff9b616
supergegs <guylachish@gmail.com>
2013-11-30 18:47:39 +0200

    Commit before push

---
+++ SGFeedReader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SGFeeder
+//
+//  Created by Guy Lachish on 10/19/13.
+//  Copyright (c) 2013 Supergegs7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
